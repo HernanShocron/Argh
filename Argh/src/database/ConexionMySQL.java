5248f72c2350e0bf93248b77c98f6f23db613803
HernanShocron <49156475+HernanShocron@users.noreply.github.com>
2019-11-22 11:31:25 -0300

    Nada imporante

--- Argh/src/database/ConexionMySQL.java
+++ Argh/src/database/ConexionMySQL.java
@@ -8,7 +8,7 @@ public class ConexionMySQL {
 	private Connection C = null;
 	private String jdbc= "jdbc:mysql://localhost:3306/tparg";
 	private String user= "root";
-	private String pass= "tekentak18";
+	private String pass= "";
 	private String driver= "com.mysql.jdbc.Driver";
 	
 	public ConexionMySQL() {
@@ -16,7 +16,7 @@ public class ConexionMySQL {
 			if (C == null){
 				Class.forName(driver);
 				C = DriverManager.getConnection(jdbc,user,pass);
-				System.out.println("Conexión establecida a la Base de Datos");	
+				System.out.println("ConexiÃ³n establecida a la Base de Datos");	
 			}				
 		}catch (SQLException | ClassNotFoundException e){
 			System.out.println("Error al conectar a la Base de Datos ");
